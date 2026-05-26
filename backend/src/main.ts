8c3df0eaa0b692af7add04d148e8c8e28beb8409
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-05-26 16:46:09 -0300

    ;

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
+  // 1. Avisamos ao NestJS que ele está usando o Express por baixo dos panos
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 2. O CORS que já arrumamos antes
   app.enableCors();
-  // Serve arquivos estáticos da pasta 'drive'
-  app.useStaticAssets(join(__dirname, '..', 'drive'), { prefix: '/files' });
-  await app.listen(process.env.PORT ?? 3000);
+  // 3. A MÁGICA: Dizemos para o NestJS liberar o acesso de leitura à pasta 'uploads'
+  // Qualquer arquivo lá dentro poderá ser visto pela URL /uploads/nome-do-arquivo.png
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  await app.listen(3000);
 }
-bootstrap();
+bootstrap(); 
